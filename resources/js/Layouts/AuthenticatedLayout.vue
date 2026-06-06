bef5daa55144274bd82a9abe2e3f42e06ee46476
yaqin66 <ainul.8815@gmail.com>
2026-06-06 16:52:49 +0700

    Menghapus Breeze dan yang berkaitan dengan login

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -50,31 +50,6 @@ const isSidebarOpen = ref(true);
                 >
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" /></svg>
                 </button>
-
-                <div class="flex items-center space-x-4">
-                    <div class="relative">
-                        <Dropdown align="right" width="48">
-                            <template #trigger>
-                                <span class="inline-flex rounded-md">
-                                    <button
-                                        type="button"
-                                        class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
-                                    >
-                                        {{ $page.props.auth.user.name }}
-                                        <svg class="-me-0.5 ms-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1.25 1.25 0 011.414 0L10 10.586l3.293-3.293a1.25 1.25 0 111.768 1.768l-4 4a1.25 1.25 0 01-1.768 0l-4-4a1.25 1.25 0 010-1.768z" clip-rule="evenodd" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </template>
-
-                            <template #content>
-                                <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                                <DropdownLink :href="route('logout')" method="post" as="button"> Log Out </DropdownLink>
-                            </template>
-                        </Dropdown>
-                    </div>
-                </div>
             </header>
 
             <div class="flex-1 overflow-x-hidden overflow-y-auto p-6 bg-gray-50">
